59b9fb8f19c29f09365da3072c92d0104a4abf8b
Orkun Karaduman <orkunkaraduman@gmail.com>
2023-07-17 12:10:24 +0300

    store: fixed unsupported perl regex

--- store/rgx.go
+++ store/rgx.go
@@ -3,6 +3,6 @@ package store
 import "regexp"
 
 var (
-	DomainRgx = regexp.MustCompile(`^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\.)+[A-Za-z]{2,6}$`)
-	HostRgx   = regexp.MustCompile(`^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\.)+[A-Za-z]{2,6}(:[0-9]{1,5})?$`)
+	DomainRgx = regexp.MustCompile(`^([A-Za-z0-9-]{1,63}\.)+[A-Za-z]{2,6}$`)
+	HostRgx   = regexp.MustCompile(`^([A-Za-z0-9-]{1,63}\.)+[A-Za-z]{2,6}(:[0-9]{1,5})?$`)
 )
