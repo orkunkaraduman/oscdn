bdf20f6fd55b7f0c2ebd09fb14de0b585a0f250b
Orkun Karaduman <orkunkaraduman@gmail.com>
2023-07-18 14:32:38 +0300

    store: implemented util funcs

---
+++ store/utils.go
@@ -0,0 +1,37 @@
+package store
+
+import (
+	"errors"
+	"io/fs"
+	"os"
+	"path"
+	"syscall"
+)
+
+func isNotEmpty(err error) bool {
+	if e := new(os.PathError); errors.As(err, &e) {
+		e, ok := e.Err.(*os.SyscallError)
+		if ok && e.Err == syscall.ENOTEMPTY {
+			return true
+		}
+	}
+	return false
+}
+
+func walkDir(root string, fn func(p string, d fs.DirEntry) bool) error {
+	return fs.WalkDir(os.DirFS(root), ".", func(p string, d fs.DirEntry, e error) error {
+		if p == "." {
+			return e
+		}
+		if e != nil {
+			if d.IsDir() && os.IsNotExist(e) {
+				return fs.SkipDir
+			}
+			return e
+		}
+		if fn(path.Join(root, p), d) {
+			return nil
+		}
+		return fs.SkipAll
+	})
+}
