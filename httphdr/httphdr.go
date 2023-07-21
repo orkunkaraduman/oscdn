97b019a6da1f7e72bc6959ca777fbe628fb96444
Orkun Karaduman <ok@orkunkaraduman.com>
2023-07-21 17:48:05 +0300

    httphdr: removed SizeOfHTTPHeader

--- httphdr/httphdr.go
+++ httphdr/httphdr.go
@@ -1,7 +1,6 @@
 package httphdr
 
 import (
-	"bytes"
 	"net/http"
 	"time"
 )
@@ -17,12 +16,6 @@ func PutHTTPHeaders(dst http.Header, src http.Header, keys ...string) {
 	for _, key := range keys {
 		dst[key] = src[key]
 	}
-}
-
-func SizeOfHTTPHeader(header http.Header) int {
-	buf := bytes.NewBuffer(nil)
-	_ = header.Write(buf)
-	return buf.Len()
 }
 
 func Expires(header http.Header, now time.Time) (expires time.Time) {
