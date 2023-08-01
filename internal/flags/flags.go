450dc80cb76b47cf421fabc1d8672a73bfe95df1
Orkun Karaduman <ok@orkunkaraduman.com>
2023-08-01 16:54:06 +0300

    flags: improved

--- internal/flags/flags.go
+++ internal/flags/flags.go
@@ -5,11 +5,13 @@ import "time"
 var Flags = &_Flags{}
 
 type _Flags struct {
-	Verbose          int           `name:"v"`
-	Debug            bool          `name:"d"`
+	Verbose          int           `default:"0"`
+	Debug            bool          `default:"false"`
 	TerminateTimeout time.Duration `default:"2m"`
 	QuitTimeout      time.Duration `default:"3m"`
+	Config           string        `default:"config.yaml"`
 	StorePath        string        `default:"."`
+	ListenBacklog    int           `default:"0"`
 	Http             string        `default:":8080"`
 	Https            string        `default:":8443"`
 	Mgmt             string        `default:":9080"`
