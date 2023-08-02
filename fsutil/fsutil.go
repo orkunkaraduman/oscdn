0af26e12614ddcaf17b382f5d7b7a1ef02fcc7d5
Orkun Karaduman <ok@orkunkaraduman.com>
2023-08-02 14:09:34 +0300

    fsutil: removed ToOSPath and WalkDir func.

--- fsutil/fsutil.go
+++ fsutil/fsutil.go
@@ -1,18 +1,11 @@
 package fsutil
 
 import (
-	"io/fs"
 	"os"
-	"path"
-	"path/filepath"
 )
 
-func ToOSPath(p string) string {
-	return filepath.FromSlash(path.Clean(p))
-}
-
 func IsExists(name string) (exists bool, err error) {
-	f, err := os.Open(ToOSPath(name))
+	f, err := os.Open(name)
 	if err != nil {
 		if os.IsNotExist(err) {
 			err = nil
@@ -29,7 +22,7 @@ func IsExists2(name string) (exists bool) {
 }
 
 func IsDir(name string) (ok bool, err error) {
-	f, err := os.Open(ToOSPath(name))
+	f, err := os.Open(name)
 	if err != nil {
 		return false, err
 	}
@@ -46,48 +39,3 @@ func IsDir2(name string) (ok bool) {
 	ok, _ = IsDir(name)
 	return
 }
-
-func walkDir(fsys fs.FS, name string, fn WalkDirFunc) (cont bool, err error) {
-	dirs, err := fs.ReadDir(fsys, name)
-	if err != nil {
-		return false, err
-	}
-
-	for _, dir := range dirs {
-		newName := path.Join(name, dir.Name())
-		if !dir.IsDir() {
-			if !fn(newName, dir) {
-				break
-			}
-			continue
-		}
-		if !fn(newName, dir) {
-			break
-		}
-		var cont1 bool
-		cont1, err = walkDir(fsys, newName, fn)
-		if err != nil {
-			return false, err
-		}
-		if !cont1 {
-			break
-		}
-	}
-
-	return false, nil
-}
-
-func WalkDir(fsys fs.FS, root string, fn WalkDirFunc) (err error) {
-	info, err := fs.Stat(fsys, root)
-	if err != nil {
-		return err
-	}
-	d := fs.FileInfoToDirEntry(info)
-	if !fn(root, d) || !d.IsDir() {
-		return nil
-	}
-	_, err = walkDir(fsys, d.Name(), fn)
-	return err
-}
-
-type WalkDirFunc func(name string, d fs.DirEntry) (cont bool)
